bb1e96bfcb86f57bcb86d28a4a9a5c6e1e6b3c2a
Michael0992 <michel.lutz92@gmail.com>
2024-11-28 09:08:19 +0100

    zurück zu Version von heute

--- Program.cs
+++ Program.cs
@@ -9,6 +9,26 @@
 
 			//Modulo gibt uns den Rest einer Division als Ganzzahl zurück. Das Zeichen für den Operanten ist %
 			//Beispiel: 4 % 2 = 0
+
+			for (int i = 1; i <= 100; i++){
+				if (i % 15 == 0)
+				{
+					Console.WriteLine("Fizzbuzz");
+				}
+				else if (i % 5 == 0) 
+				{
+                    Console.WriteLine("Buzz");
+                }
+				else if (i % 3 == 0)
+				{
+                    Console.WriteLine("Fizz");
+                }
+				else
+				{
+                    Console.WriteLine(i);
+                }
+			}
+
 		}
 	}
 }
