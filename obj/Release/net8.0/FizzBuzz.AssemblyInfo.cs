4a7290d9962d41d3d25cb694b0951b297962cf2e
Michael0992 <michel.lutz92@gmail.com>
2024-11-28 09:06:13 +0100

    zurück zu Version von gestern

--- obj/Release/net8.0/FizzBuzz.AssemblyInfo.cs
+++ obj/Release/net8.0/FizzBuzz.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("FizzBuzz")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1fbe9cb62221d904f2dbb2e475b5fee1082dcb79")]
 [assembly: System.Reflection.AssemblyProductAttribute("FizzBuzz")]
