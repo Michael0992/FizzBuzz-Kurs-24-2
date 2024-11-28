7feb44b822aea9d60abb1a2b78d690f790013bbf
Michael0992 <michel.lutz92@gmail.com>
2024-11-28 09:19:20 +0100

    letzte Sicherung

--- obj/Release/net8.0/FizzBuzz.AssemblyInfo.cs
+++ obj/Release/net8.0/FizzBuzz.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FizzBuzz")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1fbe9cb62221d904f2dbb2e475b5fee1082dcb79")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb1e96bfcb86f57bcb86d28a4a9a5c6e1e6b3c2a")]
 [assembly: System.Reflection.AssemblyProductAttribute("FizzBuzz")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FizzBuzz")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
